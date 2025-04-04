0d0a628c36632c0f29bbe4ac5af79b87adb63213
Aakash Gupta <gupta29470@gmail.com>
2025-04-04 19:59:00 +0530

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Aakash Gupta on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AfricaTests/AfricaTests.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaTests.swift
+//  AfricaTests
+//
+//  Created by Aakash Gupta on 04/04/25.
+//
+
+import Testing
+@testable import Africa
+
+struct AfricaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
